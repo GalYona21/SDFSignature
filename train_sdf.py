89dd265437acbf0a1742d56ba48c4cc65ece1a30
gal.yona <gal.yona@campus,technion.ac.il>
2024-04-07 18:22:01 +0300

    render mesh and calculate signature

--- train_sdf.py
+++ train_sdf.py
@@ -81,7 +81,7 @@ if device == 'cuda':
 # Define the loss
 loss_fn = loss.sdf
 
-root_path = os.path.join(opt.logging_root, "sdf_bunny_same_init")
+root_path = os.path.join(opt.logging_root, "sdf_bunny_same_init2")
 
 training.train(model=model, train_dataloader=dataloader, epochs=opt.num_epochs, lr=opt.lr,
                steps_til_summary=opt.steps_til_summary, epochs_til_checkpoint=opt.epochs_til_ckpt,
